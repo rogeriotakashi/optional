ef61d86a294c14c8a444a2b12bd5372fb2b37781
rogeriotakashi <rogeriotakashi@hotmail.com>
2021-04-29 13:20:36 -0300

    Added Feature A

--- src/main/java/com/rogerio/optionaldemo/examples/GitExample.java
+++ src/main/java/com/rogerio/optionaldemo/examples/GitExample.java
@@ -2,15 +2,23 @@ package com.rogerio.optionaldemo.examples;
 
 public class GitExample {
     public static void main(String[] args) {
-        System.out.println("Código da branch master");
+        System.out.println("Código da branch feature A");
         int x = 1;
         int y = 2;
         System.out.println(Calculadora.soma(x,y));
         System.out.println(Calculadora.sub(x,y));
         System.out.println(Calculadora.mult(x,y));
         System.out.println(Calculadora.div(x,y));
+
+        // Features:
+        featureA();
+    }
+
+    public static void featureA(){
+        System.out.println("Feature A implemented here!");
     }
 }
+
 
 class Calculadora {
     public static int soma(int x, int y){ return x + y; }
